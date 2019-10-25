f08119ed989d10db1dd74b96a88b1787428e2494
Tope Afolabi <topeafolabi@ymail.com>
2019-10-25 05:09:41 +0100

    add cors

--- blog/src/main/java/com/datavirtualization/blog/DataController.java
+++ blog/src/main/java/com/datavirtualization/blog/DataController.java
@@ -3,6 +3,7 @@ package com.datavirtualization.blog;
 import com.datavirtualization.blog.dao.PostRepo;
 import com.datavirtualization.blog.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class DataController {
     protected PostRepo postRepo;
 
     @RequestMapping(path="/post", produces = {"application/json"})
+    @CrossOrigin(origins = "*")
     public List<Post> getPosts(){
         return postRepo.findAll();
     }
